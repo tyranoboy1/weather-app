a169c2ceeb6c43c3784f20117cb57ac8f85950ce
JiHoon <gkwlgnd@naver.com>
2024-03-23 16:03:21 +0900

    [UPDATE] 위치정보 연동

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
+const {width: SCREEN_WIDTH } = Dimensions.get("window")
 export default function App() {
+  const [city, setCity] = useState("Loading...")
+  const [location , setLocation] = useState()
+  const [ready, setReady] = useState(true) 
+  const ask = async() => {
+    /** 사용자에게 위치 권한 물어보기 */
+    const permission = await Location.requestForegroundPermissionsAsync();
+    if(permission.granted){
+      setReady(false)
+    }
+    const {coords : {latitude,longitude }} = await Location.getCurrentPositionAsync({accuracy:5})
+    const location = await Location.reverseGeocodeAsync({latitude, longitude},{useGoogleMaps:false})
+    setCity(location[0].city)
+  }
+  useEffect(() => {
+    ask()
+  },[])
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView horizontal contentContainerStyle={styles.weather} pagingEnabled showsHorizontalScrollIndicator={false}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -31,12 +50,11 @@ const styles = StyleSheet.create({
     fontWeight:"500"
   },
   weather: {
-    flex:3,
+    backgroundColor:'red'
   },
   day:{
-    flex:1,
+    width: SCREEN_WIDTH,
     alignItems:'center',
-
   },
   temp: {
     fontSize: 158,
