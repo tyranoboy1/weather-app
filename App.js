d929e1f89b054984087833831b3a6dd8322c90d8
JiHoon <gkwlgnd@naver.com>
2024-03-23 15:31:36 +0900

    [UPDATE] 날씨 앱 레이아웃 설정

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'tomato'
   },
-});
+  city: {
+    flex:1.2,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  cityName: {
+    fontSize:68,
+    fontWeight:"500"
+  },
+  weather: {
+    flex:3,
+  },
+  day:{
+    flex:1,
+    alignItems:'center',
+
+  },
+  temp: {
+    fontSize: 158,
+    marginTop:50,
+  },
+  description: {
+    fontSize:60,
+    marginTop: -30,
+  }
+})
