7fa7a6dca59ffbc4f61e192c122fbe644a64b72e
JiHoon <gkwlgnd@naver.com>
2024-03-23 17:09:21 +0900

    [UPDATE] 날짜 api 연동완료

--- App.js
+++ App.js
@@ -1,35 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
-import * as Location from 'expo-location';
+import React, { useState, useEffect } from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import * as Location from "expo-location";
 
-const {width: SCREEN_WIDTH } = Dimensions.get("window")
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+// TODO api 키값 환경변수로 사용하는 방법 적용 해야됌 expo 환경에서
+const apiKey = "7009780a7549ccf514265937e7104785";
 export default function App() {
-  const [city, setCity] = useState("Loading...")
-  const [location , setLocation] = useState()
-  const [ready, setReady] = useState(true) 
-  const ask = async() => {
-    /** 사용자에게 위치 권한 물어보기 */
-    const permission = await Location.requestForegroundPermissionsAsync();
-    if(permission.granted){
-      setReady(false)
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ready, setReady] = useState(true);
+  const getWeather = async () => {
+    try {
+      /** 사용자에게 위치 권한 물어보기 */
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) {
+        setReady(false);
+      }
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+      const location = await Location.reverseGeocodeAsync(
+        { latitude, longitude },
+        { useGoogleMaps: false }
+      );
+      setCity(location[0].city);
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+      );
+      const json = await res.json();
+      setDays(
+        json.list.filter((weather) => {
+          if (weather.dt_txt.includes("00:00:00")) {
+            return weather;
+          }
+        })
+      );
+    } catch (e) {
+      console.log(e);
     }
-    const {coords : {latitude,longitude }} = await Location.getCurrentPositionAsync({accuracy:5})
-    const location = await Location.reverseGeocodeAsync({latitude, longitude},{useGoogleMaps:false})
-    setCity(location[0].city)
-  }
+  };
   useEffect(() => {
-    ask()
-  },[])
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
         <Text style={styles.cityName}>{city}</Text>
       </View>
-      <ScrollView horizontal contentContainerStyle={styles.weather} pagingEnabled showsHorizontalScrollIndicator={false}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+      <ScrollView
+        horizontal
+        contentContainerStyle={styles.weather}
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+      >
+        {days?.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              size="large"
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days?.map((item, idx) => {
+            return (
+              <View key={idx} style={styles.day}>
+                <Text style={styles.temp}>
+                  {parseFloat(item.main.temp).toFixed(1)}
+                </Text>
+                <Text style={styles.description}>{item.weather[0].main}</Text>
+                <Text style={styles.tinyText}>
+                  {item.weather[0].description}
+                </Text>
+              </View>
+            );
+          })
+        )}
       </ScrollView>
     </View>
   );
@@ -38,30 +93,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'tomato'
+    backgroundColor: "tomato",
   },
   city: {
-    flex:1.2,
-    justifyContent:'center',
-    alignItems:'center'
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
   },
   cityName: {
-    fontSize:68,
-    fontWeight:"500"
+    fontSize: 68,
+    fontWeight: "500",
   },
-  weather: {
-    backgroundColor:'red'
-  },
-  day:{
+  weather: {},
+  day: {
     width: SCREEN_WIDTH,
-    alignItems:'center',
+    alignItems: "center",
   },
   temp: {
     fontSize: 158,
-    marginTop:50,
+    marginTop: 50,
   },
   description: {
-    fontSize:60,
+    fontSize: 60,
     marginTop: -30,
-  }
-})
+  },
+});
